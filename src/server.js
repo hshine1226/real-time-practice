3963790c80b1e63b6cf532f416deb424e1c249ae
hshine1226 <hshine1226@gmail.com>
2022-02-24 22:50:06 +0900

    Initialize default frontend setup

--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
 import express from "express";
+
 const app = express();
 const port = 3000;
 
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("home");
 });
 
 app.listen(port, () => {
