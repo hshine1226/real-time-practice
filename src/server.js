436af9e507b5d539841c4dc57fca3054375dcda3
hshine1226 <hshine1226@gmail.com>
2022-02-24 22:57:37 +0900

    Add redirect response

--- src/server.js
+++ src/server.js
@@ -11,6 +11,10 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
+app.get("/*", (req, res) => {
+  res.redirect("/");
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
