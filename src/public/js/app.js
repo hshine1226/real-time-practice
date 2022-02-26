b13c528ba37894c28ed32b4bf6652bfa5a1af417
hshine1226 <hshine1226@gmail.com>
2022-02-26 14:25:28 +0900

    Add join room fn

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -10,11 +10,19 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const input = form.querySelector("input");
   const roomName = input.value;
+  input.value = "";
+
   socket.emit("enter_room", roomName, () => {
     welcome.hidden = true;
     room.hidden = false;
     const h3 = room.querySelector("h3");
     h3.innerText = `You are in room: ${roomName}`;
   });
-  input.value = "";
+});
+
+socket.on("welcome", () => {
+  const ul = room.querySelector("ul");
+  const li = document.createElement("li");
+  li.innerText = "Someone joined!";
+  ul.appendChild(li);
 });
